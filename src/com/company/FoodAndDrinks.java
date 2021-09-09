7ffba6818fd6f356b4ca65a2aba42d2e764bc6bf
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2021-09-09 11:55:21 -0400

    Created FoodsAndDrinks class

---
+++ src/com/company/FoodAndDrinks.java
@@ -0,0 +1,46 @@
+
+
+package com.company;
+import java.util.*;
+import java.math.*;
+
+public class FoodAndDrinks {
+    private double price;
+    private String name;
+    private String typeOfMeal;
+    private boolean isVegetarian;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeOfMeal() {
+        return typeOfMeal;
+    }
+
+    public void setTypeOfMeal(String typeOfMeal) {
+        this.typeOfMeal = typeOfMeal;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        isVegetarian = vegetarian;
+    }
+
+
+}
